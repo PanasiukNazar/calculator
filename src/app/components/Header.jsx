a36d250f41cc397e4aaec5a58d3dfd34d9a1aa05
PanasiukNazar <nazar_panasiuk.ua@gmail.com>
2022-09-11 12:34:16 +0300

    migrate to react

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from './Logo.jsx';
+import Theme from './Theme.jsx';
+
+const Header = () => {
+   return (
+      <div className='cap'>
+         <Logo />
+         <Theme />
+      </div>
+   );
+};
+
+export default Header;
---
+++ src/app/components/Theme.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ThemeChoice from './ThemeChoice.jsx';
+
+const Theme = () => {
+   return (
+      <div className='cap-theme'>
+         <div className='theme-logo'>THEME</div>
+         <ThemeChoice />
+      </div>
+   );
+};
+
+export default Theme;
